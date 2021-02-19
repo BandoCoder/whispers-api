8ee27543e41ea5b262ba6fddfc3da6497b6e5124
kittybagpipes <sparticus978@gmail.com>
2021-02-19 12:36:09 -0800

    deploy;

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
   DATABASE_URL:
     process.env.DATABASE_URL ||
     "postgres://iakfxmqwpicnuw:6585a7fe3ad68832958363148e087b373b86abf02fd361e2eb1a38e86caeb5a2@ec2-54-198-73-79.compute-1.amazonaws.com:5432/dpt2sjkg2ggnu",
-  JWT_SECRET: process.env.JWT_SECRET || "change-this.secret",
+  JWT_SECRET: process.env.JWT_SECRET || "beware-oblivion-is-at-hand",
   JWT_EXPIRY: process.env.JWT_EXPIRY || "30m",
   UNSPLASH_CLIENT_ID:
     process.env.CLIENT_ID ||
