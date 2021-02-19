d908c477d84ee76eddf76a32c4c6e0071fbb0f15
kittybagpipes <sparticus978@gmail.com>
2021-02-19 14:15:22 -0800

    deployed

--- src/config.js
+++ src/config.js
@@ -5,9 +5,7 @@ module.exports = {
     process.env.DATABASE_URL || "postgresql://jbossen@localhost/whispers",
   JWT_SECRET: process.env.JWT_SECRET || "change-this.secret",
   JWT_EXPIRY: process.env.JWT_EXPIRY || "30m",
-  UNSPLASH_CLIENT_ID:
-    process.env.CLIENT_ID ||
-    "Client-ID 8ivrf3NlYhj5r4z9Ucnv-6kcmMq5SiVvKe06zs2zfOQ",
+  UNSPLASH_CLIENT_ID: process.env.CLIENT_ID,
   UNSPLASH_URL:
     process.env.UNSPLASH_URL || "https://api.unsplash.com/search/photos",
 };
